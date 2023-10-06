1549afb3c9f9b8c583cc68622cd7ed5969bf8d21
lawrencexli <lawrencexli2@gmail.com>
2023-10-05 23:15:50 -0400

    Initialized demo service using Spring Boot

---
+++ src/main/java/com/project/ipms/IpmService.java
@@ -0,0 +1,26 @@
+/**
+ * IPMS Main Service
+ */
+package com.project.ipms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class IpmService {
+	/**
+	 * Run the service
+	 * @param args Default arguments
+	 */
+	public static void main(final String[] args) {
+		SpringApplication.run(IpmService.class, args);
+    }
+	@GetMapping("/")
+	public String hello(final @RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/project/ipms/IpmServiceTests.java
@@ -0,0 +1,13 @@
+package com.project.ipms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpmServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
