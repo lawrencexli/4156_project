88db2ebe1ff6d37d33b8c43b437b7a3892816071
lawrencexli <lawrencexli2@gmail.com>
2023-10-06 17:29:57 -0400

    Improved code styling

--- src/main/java/com/project/ipms/IpmService.java
+++ src/main/java/com/project/ipms/IpmService.java
@@ -1,6 +1,3 @@
-/**
- * IPMS Main Service
- */
 package com.project.ipms;
 
 import org.springframework.boot.SpringApplication;
@@ -13,12 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IpmService {
 	/**
-	 * Run the service
+	 * Run the service.
 	 * @param args Default arguments
 	 */
 	public static void main(final String[] args) {
 		SpringApplication.run(IpmService.class, args);
     }
+
+	/**
+	 * API call for index page.
+	 * @param name The name to be displayed
+	 * @return A string response
+	 */
 	@GetMapping("/")
 	public String hello(final @RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
