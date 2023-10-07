cd1b2fe2f744a25ca9e0ed1153ffee1af8326053
Lawrence Xiaohua Li <lawrence.lixiaohua@gmail.com>
2023-10-06 20:58:09 -0400

    Improved checkstyle

--- src/main/java/com/project/ipms/IpmService.java
+++ src/main/java/com/project/ipms/IpmService.java
@@ -9,21 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class IpmService {
-	/**
-	 * Run the service.
-	 * @param args Default arguments
-	 */
-	public static void main(final String[] args) {
-		SpringApplication.run(IpmService.class, args);
+    /**
+     * Run the service.
+     * @param args Default arguments
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(IpmService.class, args);
     }
 
-	/**
-	 * API call for index page.
-	 * @param name The name to be displayed
-	 * @return A string response
-	 */
-	@GetMapping("/")
-	public String hello(final @RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+    /**
+     * API call for index page.
+     * @param name The name to be displayed
+     * @return A string response
+     */
+    @GetMapping("/")
+    public String hello(final @RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
