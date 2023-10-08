f714ddbd2cc9a1ff96b33382cfd692c32c093ed3
Lawrence Xiaohua Li <lawrence.lixiaohua@gmail.com>
2023-10-08 19:32:06 -0400

    Further fixed formatting issues in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ extra["springCloudVersion"] = "2022.0.4"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.google.cloud:spring-cloud-gcp-starter-storage")
-    implementation("com.github.spotbugs:spotbugs-annotations:4.0.1")
+	implementation("com.github.spotbugs:spotbugs-annotations:4.0.1")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
