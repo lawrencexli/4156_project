120cc5bbaf8804040fa28ac8af2011e53963df4f
Lawrence Xiaohua Li <lawrence.lixiaohua@gmail.com>
2023-11-26 12:55:09 -0500

    Enabling CORS for all API endpoints

--- src/main/java/com/project/ipms/controller/FileController.java
+++ src/main/java/com/project/ipms/controller/FileController.java
@@ -17,7 +17,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
