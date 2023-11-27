1121d6bbd4214fd4d8a47af47429c8a8c5dab1e7
Lawrence Xiaohua Li <lawrence.lixiaohua@gmail.com>
2023-11-27 11:19:40 -0500

    Download API endpoint will no longer need body parameters.

--- src/main/java/com/project/ipms/controller/FileController.java
+++ src/main/java/com/project/ipms/controller/FileController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
@@ -112,8 +113,8 @@ public class FileController {
      */
     @CrossOrigin
     @GetMapping("download")
-    public ResponseEntity<Resource> downloadFile(@RequestPart final String fileName,
-                                                 @RequestPart final String id) {
+    public ResponseEntity<Resource> downloadFile(@RequestParam final String fileName,
+                                                 @RequestHeader final String id) {
         if (fileName == null || fileName.isBlank()) {
             throw new BadRequestException("Filename is empty or null");
         }
