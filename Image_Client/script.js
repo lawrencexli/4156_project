2accfdbac8e1c92dc5c8da7a36c46aa522a1afb5
Cole Donat <cole.donat@gmail.com>
2023-12-17 23:42:59 -0500

    Added image preview functionality

--- Image_Client/script.js
+++ Image_Client/script.js
@@ -76,6 +76,17 @@ $(function () {
     });
 });
 
+const previewImage = (event) => {
+    const imageFiles = event.target.files;
+    const imageFilesLength = imageFiles.length;
+    if (imageFilesLength > 0) {
+        const imageSrc = URL.createObjectURL(imageFiles[0]);
+        const imagePreviewElement = document.querySelector("#preview-selected-image");
+        imagePreviewElement.src = imageSrc;
+        imagePreviewElement.style.display = "block";
+    }
+};
+
 async function generate_id() {
     const response = await fetch("http://localhost:8080/api/generate");
     const data = await response.json();
